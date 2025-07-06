bc66dcd3ee9780208b1b0e5f233ebb280a597e87
Osmar Hofman <hofman.osmar@gmail.com>
2025-07-06 20:40:11 -0300

    feat: Adicionado opções de ramping

--- K6App/index.js
+++ K6App/index.js
@@ -1,16 +1,26 @@
-import GetAllProducts, {
-  GetProductDuration,
-} from "./scenarios/GetAllProducts.js";
-import { group, sleep } from "k6";
+import GetAllProducts from './scenarios/GetAllProducts.js';
+import { group, sleep } from 'k6';
 
+// O trecho abaixo é opcional, mas pode ser usado para definir o número de usuários virtuais (vus) e a duração do teste.
+
+// export const options = {
+//   vus: 10,
+//   duration: "30s",
+// };
+
+// O trecho abaixo é baseado na configuração de Ramping UVs: https://grafana.com/docs/k6/latest/using-k6/scenarios/executors/ramping-vus/
+// No caso abaixo, o teste começará com 20 usuários virtuais (vus) por 5 segundos, depois reduzirá para 10 vus por 10 segundos e finalmente terminará com 0 vus após 15 segundos.
 export const options = {
-  vus: 10,
-  duration: "30s",
+  stages: [
+    { duration: '5s', target: 20 },
+    { duration: '10s', target: 10 },
+    { duration: '15s', target: 0 },
+  ],
 };
 
 export default () => {
   group(
-    "Endpoint Get All Products - Controller Products - Products.Api",
+    'Endpoint Get All Products - Controller Products - Products.Api',
     () => {
       GetAllProducts();
     }
--- K6App/scenarios/GetAllProducts.js
+++ K6App/scenarios/GetAllProducts.js
@@ -1,16 +1,16 @@
-import * as http from "k6/http";
-import { check, fail, sleep } from "k6";
-import { Trend, Rate } from "k6/metrics";
+import * as http from 'k6/http';
+import { check, fail, sleep } from 'k6';
+import { Trend, Rate } from 'k6/metrics';
 
-export let GetProductDuration = new Trend("get_product_duration");
-export let GetProductFailRate = new Rate("get_product_fail_rate");
-export let GetProductSuccessRate = new Rate("get_product_success_rate");
-export let GetProductReqs = new Rate("get_product_reqs");
+export let GetProductDuration = new Trend('get_product_duration');
+export let GetProductFailRate = new Rate('get_product_fail_rate');
+export let GetProductSuccessRate = new Rate('get_product_success_rate');
+export let GetProductReqs = new Rate('get_product_reqs');
 
 export default function () {
-  console.log("valor http: ", http);
+  console.log('valor http: ', http);
 
-  let res = http.get("http://localhost:5214/GetAllProducts");
+  let res = http.get('http://localhost:5214/GetAllProducts');
 
   GetProductDuration.add(res.timings.duration);
   GetProductReqs.add(1);
@@ -20,7 +20,7 @@ export default function () {
   let durationMessage = `Duração máxima ${4000 / 1000}s`;
   if (
     !check(res, {
-      "max duration": (r) => r.timings.duration < 4000,
+      'max duration': (r) => r.timings.duration < 4000,
     })
   ) {
     fail(`A requisição demorou mais do que ${durationMessage}`);
