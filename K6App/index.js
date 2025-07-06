058bddac9c1d85d521b53d9d94eace124aaa7fc8
Osmar Hofman <hofman.osmar@gmail.com>
2025-07-06 20:58:38 -0300

    feat: Adicionado opção de iterações

--- K6App/index.js
+++ K6App/index.js
@@ -8,14 +8,15 @@ import { group, sleep } from 'k6';
 //   duration: "30s",
 // };
 
-// O trecho abaixo é baseado na configuração de Ramping UVs: https://grafana.com/docs/k6/latest/using-k6/scenarios/executors/ramping-vus/
-// No caso abaixo, o teste começará com 20 usuários virtuais (vus) por 5 segundos, depois reduzirá para 10 vus por 10 segundos e finalmente terminará com 0 vus após 15 segundos.
 export const options = {
+  // O trecho abaixo é baseado na configuração de Ramping UVs: https://grafana.com/docs/k6/latest/using-k6/scenarios/executors/ramping-vus/
+  // No caso abaixo, o teste começará com 20 usuários virtuais (vus) por 5 segundos, depois reduzirá para 10 vus por 10 segundos e finalmente terminará com 0 vus após 15 segundos.
   stages: [
     { duration: '5s', target: 20 },
     { duration: '10s', target: 10 },
     { duration: '15s', target: 0 },
   ],
+  iterations: 10, // Define o número de iterações que cada usuário virtual executará
 };
 
 export default () => {
